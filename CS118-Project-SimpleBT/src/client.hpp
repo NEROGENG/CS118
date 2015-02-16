1b31fadcf8accbeeb3b65276be1eb9bcaaffe3d9
NEROGENG <szmun.gengqj@gmail.com>
2015-02-16 15:12:55 -0800

    kill or not kill. thats a good question

--- CS118-Project-SimpleBT/src/client.hpp
+++ CS118-Project-SimpleBT/src/client.hpp
@@ -32,133 +32,137 @@
 #include <map>
 
 namespace sbt {
-
-class Client
-{
-public:
-  class Error : public std::runtime_error
-  {
-  public:
-    explicit
-    Error(const std::string& what)
-      : std::runtime_error(what)
+    
+    class Client
     {
-    }
-  };
+    public:
+        class Error : public std::runtime_error
+        {
+        public:
+            explicit
+            Error(const std::string& what)
+            : std::runtime_error(what)
+            {
+            }
+        };
+        
+    public:
+        Client(const std::string& port,
+               const std::string& torrent);
+        
+        void
+        run();
+        
+        const std::string&
+        getTrackerHost() {
+            return m_trackerHost;
+        }
+        
+        const std::string&
+        getTrackerPort() {
+            return m_trackerPort;
+        }
+        
+        const std::string&
+        getTrackerFile() {
+            return m_trackerFile;
+        }
+        
+    private:
+        void
+        loadMetaInfo(const std::string& torrent);
+        
+        void
+        connectTracker();
+        
+        void
+        sendTrackerRequest();
+        
+        bool
+        checkFile(const std::string& filename);
+        
+        void
+        checkFileOrCreate();
+        
+        TrackerResponse
+        recvTrackerResponse(bool& good_response);
+        
+        void
+        broadcastHaveMsg(int pieceIndex);
+        
+        bool
+        requestPiece(int pieceIndex);
+        
+        void
+        readPieceToBuffer(char* buffer, int pieceIndex, int pieceLength);
+        
+        void
+        writePieceToDisk(const char* buffer, int pieceIndex, int pieceLength);
+        
+        bool
+        validatePiece(const std::string& text, const std::string& hash);
+        
+        bool hasPiece(std::vector<uint8_t> bitfield, int pieceIndex);
+        
+        void
+        initializePeerBitfield(std::string peerid, std::vector<uint8_t> bitfield);
+        
+        void
+        updatePeerBitfield(std::string peerid, int pieceIndex);
+        
+        void
+        updateTrackerRequest(int up, int down);
+        
+        void
+        updateSelfBitfield(int pieceIndex);
+        
+        int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y);
+        
+        const std::string getMyIP();
+        
+    private:
+        MetaInfo m_metaInfo;
+        std::string m_trackerHost;
+        std::string m_trackerPort;
+        std::string m_trackerFile;
+        std::string m_myIP;
+        
+        uint16_t m_clientPort;
+        
+        int m_trackerSock;
+        int m_serverSock = -1;
+        
+        fd_set m_readSocks;
+        
+        uint64_t m_interval;
+        bool m_isFirstReq;
+        bool m_isFirstRes;
 
-public:
-  Client(const std::string& port,
-         const std::string& torrent);
-
-  void
-  run();
-
-  const std::string&
-  getTrackerHost() {
-    return m_trackerHost;
-  }
-
-  const std::string&
-  getTrackerPort() {
-    return m_trackerPort;
-  }
-
-  const std::string&
-  getTrackerFile() {
-    return m_trackerFile;
-  }
-
-private:
-  void
-  loadMetaInfo(const std::string& torrent);
-
-  void
-  connectTracker();
-
-  void
-  sendTrackerRequest();
-
-    bool
-  checkFile(const std::string& filename);
-
-  void
-  checkFileOrCreate();
-
-  TrackerResponse
-  recvTrackerResponse(bool& good_response);
-
-  void
-  broadcastHaveMsg(int pieceIndex);
-
-  bool
-  requestPiece(int pieceIndex);
-
-  void 
-  readPieceToBuffer(char* buffer, int pieceIndex, int pieceLength);
-
-  void 
-  writePieceToDisk(const char* buffer, int pieceIndex, int pieceLength);
-
-  bool 
-  validatePiece(const std::string& text, const std::string& hash);
- 
-  bool hasPiece(std::vector<uint8_t> bitfield, int pieceIndex);
-
-  void
-  initializePeerBitfield(std::string peerid, std::vector<uint8_t> bitfield);
-
-  void
-  updatePeerBitfield(std::string peerid, int pieceIndex);
-
-  void
-  updateTrackerRequest(int up, int down);
-
-  void
-  updateSelfBitfield(int pieceIndex);
-
-  const std::string getMyIP();
-
-private:
-  MetaInfo m_metaInfo;
-  std::string m_trackerHost;
-  std::string m_trackerPort;
-  std::string m_trackerFile;
-  std::string m_myIP;
-
-  uint16_t m_clientPort;
-
-  int m_trackerSock;
-  int m_serverSock = -1;
-
-  fd_set m_readSocks;
-
-  uint64_t m_interval;
-  bool m_isFirstReq;
-  bool m_isFirstRes;
-  bool m_isComplete;
-
-  int m_numPieces;
-  std::vector<uint8_t> m_bitfield;
-  std::vector<PeerInfo> m_peerlist;
-  std::map<std::string, int> m_connectionlist;
-  std::vector<int> m_listen_list;
-  std::map<std::string, std::vector<uint8_t> > m_peerbitfields;
-  std::set<int> m_requested;
-  std::map<int, std::string> m_inverseList;
-  std::vector<std::pair<int, int>> m_fdToPieceNum;
-
-  std::map<int, std::vector<size_t>> m_pieceNeed;
-
-  std::map<int, bool> m_unchoke;
-
-  int m_downloaded;
-  int m_uploaded;
-  int m_left;
-
-  bool good_response;
-
-};
-
+        
+        int m_numPieces;
+        std::vector<uint8_t> m_bitfield;
+        std::vector<PeerInfo> m_peerlist;
+        std::map<std::string, int> m_connectionlist;
+        std::vector<int> m_listen_list;
+        std::map<std::string, std::vector<uint8_t> > m_peerbitfields;
+        std::set<int> m_requested;
+        std::map<int, std::string> m_inverseList;
+        std::vector<std::pair<int, int>> m_fdToPieceNum;
+        
+        std::map<int, std::vector<size_t>> m_pieceNeed;
+        
+        std::map<int, bool> m_unchoke;
+        
+        
+        int m_downloaded;
+        int m_uploaded;
+        bool m_isComplete;
+        int m_left;
+        
+        bool good_response;
+        
+    };
+    
 } // namespace sbt
 
 #endif // SBT_CLIENT_HPP
